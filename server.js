1c1187278e49ff524b081e9fd2e67bbf776445cf
Rishabh Agrawal <ragrawal804@gmail.com>
2017-01-04 21:02:25 +0530

    initial angular node scarefolding

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+app.set('port',3000)
+app.use('/public',express.static(__dirname+'/public'))
+app.use('/bower_components',express.static(__dirname+'/bower_components'))
+app.get('/',function(req,res){
+    res.sendFile('index.html',{root:__dirname+'/public'})
+})
+app.listen(app.get('port'),function(err){
+    console.log("App is Running At PORT NO: ",app.get('port'));
+})

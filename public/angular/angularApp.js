8b20b0c3acc675e37068550cc627818ec15c779e
Rishabh Agrawal <ragrawal804@gmail.com>
2017-02-14 19:54:24 +0530

    basic alignment

--- public/angular/angularApp.js
+++ public/angular/angularApp.js
@@ -5,5 +5,5 @@ angular
 HomeController.$inject = ['$scope','$http']
 
 function HomeController($scope,$http) {
-    $scope.test = "This is test Text"
+    $scope.test = "This is test Text And Your Angular App is working Fine now let's add some more code this is only a wrapper"
 }
